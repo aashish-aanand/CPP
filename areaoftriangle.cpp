9aae5704b3b9c6e50bd776596c4e56defdf072c1
Ashish Anand <111524396+aashish-aanand@users.noreply.github.com>
2023-05-04 23:23:01 +0530

    Add files via upload

---
+++ areaoftriangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,h;
+    cout<<"Enter base ";
+    cin>>b;
+    cout<<"Enter height ";
+    cin>>h;
+    a=(b*h)/2;
+    cout<<"Area is "<<a;
+    return 0;
+    
+}
