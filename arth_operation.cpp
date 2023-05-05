e4d957572f3c420117daeb49cce66dd00255fecc
Ashish Anand <111524396+aashish-aanand@users.noreply.github.com>
2023-05-05 20:20:59 +0530

    Add files via upload

---
+++ arth_operation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,e,f,g;
+    cout<<"Enter first no ";
+    cin>>a;
+    cout<<"Enter second no ";
+    cin>>b;
+    c=a+b;
+    cout<<"Addition "<<c<<endl;
+    d=a-b;
+    cout<<"Subtraction "<<d<<endl;
+    e=a*b;
+    cout<<"Multiplication "<<e<<endl;
+    f=a/b;
+    cout<<"Division "<<f<<endl;
+    g=a%b;
+    cout<<"Remainder "<<g<<endl;
+    return 0; 
+}
