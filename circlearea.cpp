d7f704f0f6d3a4cd73624af5e9d86291faa5d4fb
Ashish Anand <111524396+aashish-aanand@users.noreply.github.com>
2023-05-05 20:23:23 +0530

    Add files via upload

---
+++ circlearea.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter radius ";
+    float area, r;
+    cin>>r;
+    area = 3.1425f*(r*r);
+    cout<<"Area is "<<area;
+    return 0;
+}
