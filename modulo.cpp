b1e8b5d449719eb6ac9c4e84bf036f854119fc55
Ashish Anand <111524396+aashish-aanand@users.noreply.github.com>
2023-05-07 20:15:51 +0530

    Add files via upload

---
+++ modulo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>     
+using namespace std;
+int main(){
+    int a=13, b=5, c;
+    float d;
+    c=a%b;
+    d=(float)a/b;
+    cout<<c<<endl<<d;
+
+    char e=13, f=15, g, h;
+    g=e%f;
+    h=e/f;
+    cout<<g<<endl<<h;
+
+
+
+    return 0;
+
+
+}
