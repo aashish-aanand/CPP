c342f9290cdaa310fc6ce1b87e0ffc320f1ebb61
Ashish Anand <111524396+aashish-aanand@users.noreply.github.com>
2023-05-11 17:53:10 +0530

    Add files via upload

---
+++ typedef.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+typedef int marks;
+int main()
+{
+    marks m1;
+    m1=5;
+    // we declared a data type named marks and it is of integer data type
+    //here m1, m2... are variables of marks data type which is of integer data type
+    cout<<m1;
+    return 0;
+}
