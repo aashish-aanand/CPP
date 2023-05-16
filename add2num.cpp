972c6b0627fb4b7d7a744fc389365f2f187ac34b
Ashish Anand <111524396+aashish-aanand@users.noreply.github.com>
2023-05-16 17:14:24 +0530

    Update add2num.cpp

--- add2num.cpp
+++ add2num.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a, b, c;
-    cout<<"Enter 2 nos ";
-    cin>>a>>b;
-    c=a+b;
-    cout<<"Addition "<<c;
+    int a, b, c, d;
+    cout<<"Enter 3 nos ";
+    cin>>a>>b>>c;
+    d=a+b+c;
+    cout<<"Addition "<<d;
     return 0;
 }
